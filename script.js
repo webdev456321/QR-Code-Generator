4d1078fc98690745049366d59bb87816a7fe1d15
webdev9732145 <116497274+webdev456321@users.noreply.github.com>
2023-08-16 19:50:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let input1=document.getElementsByTagName('input')[0];
+let form1=document.getElementsByTagName('form')[0]
+let img1=document.createElement('img')
+let p=document.getElementsByTagName('p')[0]
+let copyImgBtn=document.createElement('button')
+copyImgBtn.textContent="Copy Image"
+
+form1.addEventListener('submit',(e)=>{
+e.preventDefault()
+
+if(input1.value===''){
+   p.textContent='Enter data!'
+   form1.removeChild(img1)
+   form1.removeChild(copyImgBtn)
+}
+else{
+img1.setAttribute('src',`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input1.value}`)
+form1.appendChild(img1)
+img1.setAttribute('style','height:61%')
+img1.style.paddingTop='5px'
+form1.style.paddingBottom='100px'
+img1.style.borderRadius='10px'
+p.textContent='Scan the QR with your phone'
+form1.appendChild(copyImgBtn)
+
+}
+
+})
+
+copyImgBtn.addEventListener('click',()=>{
+    var range=document.createRange()
+    range.selectNode(img1)
+    window.getSelection().addRange(range);
+
+    try{
+        document.execCommand('copy')
+    }
+    catch(err){
+        console.log(msg)
+    }
+    
+    window.getSelection().removeAllRanges()
+}
+)
